091cb9484ee72cb83341cd97874b41799c1d97c3
Andreas Wieland <azak@protonmail.com>
2020-06-20 15:08:50 +0200

    Changed logging to zap Library

--- utils/client_test.go
+++ utils/client_test.go
@@ -2,24 +2,22 @@ package utils
 
 import (
 	"context"
-	"github.com/azak-azkaran/goproxy"
 	"net"
 	"net/http"
-	"os"
 	"testing"
 	"time"
+
+	"github.com/azak-azkaran/goproxy"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestGetResponse(t *testing.T) {
-	Init(os.Stdout, os.Stdout, os.Stderr)
+	Init()
 
 	resp, err := GetResponse("", "https://www.google.de")
-	if err != nil {
-		t.Error("Error while requesting without proxy, ", err)
-	}
-	if resp.StatusCode != 200 {
-		t.Error("Google could not be requested, ", resp.Status)
-	}
+	require.NoError(t, err)
+	assert.Equal(t, http.StatusOK, resp.StatusCode)
 
 	proxy := goproxy.NewProxyHttpServer()
 
@@ -29,41 +27,30 @@ func TestGetResponse(t *testing.T) {
 
 	var server *http.Server
 	go func() {
-		Init(os.Stdout, os.Stdout, os.Stderr)
-		Info.Println("serving end proxy server at localhost:8082")
+		Init()
+		Sugar.Info("serving end proxy server at localhost:8082")
 		server = &http.Server{
 			Addr:    "localhost:8082",
 			Handler: proxy,
 		}
 		err := server.ListenAndServe()
-		if err != http.ErrServerClosed {
-			t.Error("Other Error then ServerClose", err)
-		}
+		assert.Error(t, err)
+		assert.NotEqual(t, http.ErrServerClosed, err)
 	}()
 
 	time.Sleep(1 * time.Second)
 	resp, err = GetResponse("http://localhost:8082", "https://www.google.de")
-	if err != nil {
-		t.Error("Error while requesting without proxy, ", err)
-	}
-	if resp.StatusCode != 200 {
-		t.Error("Google could not be requested, ", resp.Status)
-	}
+	require.NoError(t, err)
+	assert.Equal(t, http.StatusOK, resp.StatusCode)
 
 	err = server.Shutdown(context.TODO())
-	if err != nil {
-		t.Error("Error while shutting down server")
-	}
+	assert.NoError(t, err)
 }
 
 func TestGetResponseDump(t *testing.T) {
-	Init(os.Stdout, os.Stdout, os.Stderr)
+	Init()
 
 	dump, err := GetResponseDump("", "https://www.google.de")
-	if err != nil {
-		t.Error("Error while requesting without proxy, ", err)
-	}
-	if len(dump) == 0 {
-		t.Error("Google response was empty")
-	}
+	assert.NoError(t, err)
+	assert.Len(t, dump, 0)
 }
--- utils/log_test.go
+++ utils/log_test.go
@@ -1,88 +1,118 @@
 package utils
 
 import (
-	"bytes"
 	"fmt"
-	"io/ioutil"
-	"os"
-	"strings"
 	"testing"
+	"time"
+
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 )
 
 func TestInit(t *testing.T) {
 
 	message := "message"
-	var infobuffer bytes.Buffer
-	var warningbuffer bytes.Buffer
-	var errorbuffer bytes.Buffer
-	Init(&infobuffer, &warningbuffer, &errorbuffer)
+	Init()
 
-	Info.Println(message)
-	s := infobuffer.String()
+	Sugar.Info(message)
+	Sugar.Error(message)
 
-	if !strings.Contains(s, "INFO") {
-		t.Errorf("INFO Buffer does not contain INFO keyword\n%s", s)
-	}
-	if !strings.Contains(s, message) {
-		t.Errorf("INFO Buffer does not contain message keyword\n%s", s)
-	}
-	if strings.Contains(s, "ERROR") {
-		t.Errorf("INFO Buffer does contain ERROR keyword\n%s", s)
-	}
+	//Info.Println(message)
+	//s := infobuffer.String()
 
-	Error.Println(message)
-	s = errorbuffer.String()
+	//if !strings.Contains(s, "INFO") {
+	//	t.Errorf("INFO Buffer does not contain INFO keyword\n%s", s)
+	//}
+	//if !strings.Contains(s, message) {
+	//	t.Errorf("INFO Buffer does not contain message keyword\n%s", s)
+	//}
+	//if strings.Contains(s, "ERROR") {
+	//	t.Errorf("INFO Buffer does contain ERROR keyword\n%s", s)
+	//}
 
-	if !strings.Contains(s, "ERROR") {
-		t.Errorf("ERROR Buffer does not contain ERROR keyword\n%s", s)
-	}
-	if !strings.Contains(s, message) {
-		t.Errorf("ERROR Buffer does not contain message keyword\n%s", s)
-	}
-	if strings.Contains(s, "INFO") {
-		t.Errorf("ERROR Buffer does contain INFO keyword\n%s", s)
-	}
+	//Error.Println(message)
+	//s = errorbuffer.String()
+
+	//if !strings.Contains(s, "ERROR") {
+	//	t.Errorf("ERROR Buffer does not contain ERROR keyword\n%s", s)
+	//}
+	//if !strings.Contains(s, message) {
+	//	t.Errorf("ERROR Buffer does not contain message keyword\n%s", s)
+	//}
+	//if strings.Contains(s, "INFO") {
+	//	t.Errorf("ERROR Buffer does contain INFO keyword\n%s", s)
+	//}
+
+	//sugar.Infof("test: %s", "blub")
 }
 
 func TestSetLogPath(t *testing.T) {
 	fmt.Println("Running: TestSetLogPath")
-	message := "message"
-	path := "testInfoBuffer"
+	return
 
-	buffer := SetLogPath(path)
-	LogFile = buffer
+	/*
+		message := "message"
+		path := "testInfoBuffer"
+			buffer := SetLogPath(path)
+			LogFile = buffer
 
-	Info.Println(message)
-	Warning.Println(message)
-	Error.Println(message)
+			Info.Println(message)
+			Warning.Println(message)
+			Error.Println(message)
 
-	err := LogFile.Close()
-	if err != nil {
-		t.Errorf("%s could not be closed: %s", path, err)
-	}
-	dat, err := ioutil.ReadFile(path)
-	if err != nil {
-		t.Errorf("error opening file: %v", err)
-	}
+			err := LogFile.Close()
+			if err != nil {
+				t.Errorf("%s could not be closed: %s", path, err)
+			}
+			dat, err := ioutil.ReadFile(path)
+			if err != nil {
+				t.Errorf("error opening file: %v", err)
+			}
 
-	m := string(dat)
-	if !strings.Contains(m, message) {
-		t.Error("File does not contain message")
-	}
+			m := string(dat)
+			if !strings.Contains(m, message) {
+				t.Error("File does not contain message")
+			}
 
-	if !strings.Contains(m, "INFO") {
-		t.Error("File does not contain INFO message")
-	}
-	if !strings.Contains(m, "WARNING") {
-		t.Error("File does not contain WARNING message: ", m)
-	}
-	if !strings.Contains(m, "ERROR") {
-		t.Error("File does not contain ERROR message")
-	}
+			if !strings.Contains(m, "INFO") {
+				t.Error("File does not contain INFO message")
+			}
+			if !strings.Contains(m, "WARNING") {
+				t.Error("File does not contain WARNING message: ", m)
+			}
+			if !strings.Contains(m, "ERROR") {
+				t.Error("File does not contain ERROR message")
+			}
 
-	err = os.Remove(path)
-	if err != nil {
-		t.Errorf("%s could not be deleted", path)
-	}
+			err = os.Remove(path)
+			if err != nil {
+				t.Errorf("%s could not be deleted", path)
+			}
+	*/
+}
 
+func TestZapLogger(t *testing.T) {
+	fmt.Println("Running: TestZapLogger")
+
+	atom := zap.NewAtomicLevel()
+	config := zap.NewProductionConfig()
+	config.EncoderConfig.EncodeLevel = zapcore.CapitalColorLevelEncoder
+	config.EncoderConfig.EncodeTime = zapcore.RFC3339TimeEncoder
+	config.Encoding = "console"
+	config.Level = atom
+
+	logger, _ := config.Build()
+
+	url := "www.google.de"
+	defer logger.Sync() // flushes buffer, if any
+	sugar := logger.Sugar()
+	atom.SetLevel(zap.WarnLevel)
+	sugar.Info("Now logs should be colored")
+	sugar.Warnw("failed to fetch URL",
+		// Structured context as loosely typed key-value pairs.
+		"url", url,
+		"attempt", 3,
+		"backoff", time.Second,
+	)
+	sugar.Errorf("Failed to fetch URL: %s", url)
 }
